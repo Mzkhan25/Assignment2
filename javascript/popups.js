f9e4333dd698fb6876e89c6425acc3253467a320
Mzkhan25 <mzkhan25@hotmail.com>
2015-11-20 21:31:29 +0500

    First commit

---
+++ javascript/popups.js
@@ -0,0 +1,9 @@
+﻿function popup(name, width, height) {
+    var options = "toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=0,resizable=0,width=" + width + ",height=" + height;
+    Cal2 = window.open(name, "popup", options);
+}
+
+function popupclock(name, width, height) {
+    var options = "toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=0,resizable=0,width=" + width + ",height=" + height;
+    Cal2 = window.open(name, "popup", options);
+}
